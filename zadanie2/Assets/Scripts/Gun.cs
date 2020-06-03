e596d9d1a945e71d2063041b81781057cb627577
prytel97 <61902313+prytel97@users.noreply.github.com>
2020-06-04 00:36:45 +0200

    a

---
+++ zadanie2/Assets/Scripts/Gun.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+	public Rigidbody Bullet;
+	public Transform firePlace;
+	private Rigidbody projectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+		{
+			projectile = Instantiate(Bullet, firePlace.position, transform.rotation);
+		}
+    }
+}
