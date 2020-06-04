f01441aabba93b9ffe5b38dd8720b2c323983c37
prytel97 <61902313+prytel97@users.noreply.github.com>
2020-06-04 22:37:51 +0200

    a

--- zadanie2/Assets/Scripts/Gun.cs
+++ zadanie2/Assets/Scripts/Gun.cs
@@ -6,7 +6,9 @@ public class Gun : MonoBehaviour
 {
 	public Rigidbody Bullet;
 	public Transform firePlace;
+	public float power;
 	private Rigidbody projectile;
+	private bool fire;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +21,17 @@ public class Gun : MonoBehaviour
         if(Input.GetButtonDown("Fire1"))
 		{
 			projectile = Instantiate(Bullet, firePlace.position, transform.rotation);
+			fire = true;
 		}
     }
+	
+	private void FixedUpdate()
+	{
+		if(fire)
+		{
+			projectile.velocity = transform.TransformDirection(Vector3.forward * power);
+			fire = false;
+			Destroy(projectile.gameObject, 3);
+		}
+	}
 }
